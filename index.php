c8dd6c5a2ac614a0c846ee1d28257fb368935ea1
Dustin Leach <ldust667@gmail.com>
2018-12-21 10:42:51 -0500

    Initial commit
    Correctly connected to local mysql database.

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta charset="UTF-8">
+  <meta name="description" content="Test database">
+  <meta name="keywords" content="HTML,CSS,XML,JavaScript">
+  <meta name="author" content="Dustin Leach">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP test</title>
+</head>
+
+
+<body>
+
+
+
+<?php
+
+$user = 'dleach';
+$pass = 'newpass';
+
+// connect to the database
+
+
+
+// select a database:
+
+try{
+
+$conn = new PDO('mysql:host=localhost;dbname=prices', $user, $pass);
+
+echo "hello";
+ foreach($conn->query('SELECT * from prices') as $row) {
+        print "<p>" . $row . "</p>";
+    }
+
+}
+
+catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+
+
+/*mysql_select_db('prices')
+or die('Could not select a database.');
+// build query:
+$sql = "SELECT name, price_usd_lb FROM metals";
+// execute query:
+$result = mysql_query($sql)
+or die('A error occurred: ' . mysql_error());
+// get result count:
+$count = mysql_num_rows($result);
+print "<h3>$count metal prices available</h3>";
+print "<table>";
+print "<tr><th>Name</th><th>Price (USD/lb)</th></tr>";
+// fetch results:
+while ($row = mysql_fetch_assoc($result)) {
+$name = $row['name'];
+$price = $row['price_usd_lb'];
+print "<tr><td><strong>$name</strong></td><td>$price</td></tr>";
+}
+print "</table>";
+*/
+?>
+
+</body>
+
+</html>
