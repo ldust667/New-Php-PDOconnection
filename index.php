a9270aa8199410aadd3ea974687c945ea5acdc34
Dustin Leach <ldust667@gmail.com>
2018-12-21 11:46:58 -0500

    Parsing out
    Parsed out the fetch all method. Fetchall returns a two demensional array.

--- index.php
+++ index.php
@@ -30,11 +30,23 @@ try{
 //connecting to the database
 $conn = new PDO('mysql:host=localhost;dbname=prices', $user, $pass);
 
-//printing as a test, output notworking from loop
-echo "hello";
- foreach($conn->query('SELECT * from metals') as $row) {
-        print "<p>" . $row . "</p>";
-    }
+
+
+//$sqlst = $conn->prepare("select count(*) from metals");
+//$sqlst->execute();
+//print $sqlst;
+
+
+//select query for output
+$sqlst = $conn->prepare("select name from metals");
+$sqlst->execute();
+
+$result = $sqlst->fetchAll();
+
+
+print($result[0][0]); 
+
+
 
 }
 
@@ -45,26 +57,6 @@ catch (PDOException $e) {
 }
 
 
-/*mysql_select_db('prices')
-or die('Could not select a database.');
-// build query:
-$sql = "SELECT name, price_usd_lb FROM metals";
-// execute query:
-$result = mysql_query($sql)
-or die('A error occurred: ' . mysql_error());
-// get result count:
-$count = mysql_num_rows($result);
-print "<h3>$count metal prices available</h3>";
-print "<table>";
-print "<tr><th>Name</th><th>Price (USD/lb)</th></tr>";
-// fetch results:
-while ($row = mysql_fetch_assoc($result)) {
-$name = $row['name'];
-$price = $row['price_usd_lb'];
-print "<tr><td><strong>$name</strong></td><td>$price</td></tr>";
-}
-print "</table>";
-*/
 ?>
 
 </body>
